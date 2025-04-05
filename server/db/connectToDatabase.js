c1ac953b1e3f3a07a37f84d6824ea87b52552747
varun <varunchowdary23@gmail.com>
2025-04-05 11:29:35 +0530

    env name fix

--- server/db/connectToDatabase.js
+++ server/db/connectToDatabase.js
@@ -6,7 +6,7 @@ const connectToDB = async () => {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
     isConnected = true;
     console.log('Database connected');
